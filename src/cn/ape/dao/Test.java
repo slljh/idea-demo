4ff34402a867ac8eda4d8812f68f18ffd9e21d34
sl <562650878@qq.com>
2019-08-19 19:55:26 +0800

    第一次提交项目代码

---
+++ src/cn/ape/dao/Test.java
@@ -0,0 +1,9 @@
+package cn.ape.dao;
+
+import com.alibaba.fastjson.JSON;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(JSON.toJSON("SS"));
+    }
+}
---
+++ src/cn/ape/dao/UserServlet.java
@@ -0,0 +1,13 @@
+package cn.ape.dao;
+
+import java.io.IOException;
+
+public class UserServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        response.getWriter().write("hello Servlet!");
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        doPost(request,response);
+    }
+}
